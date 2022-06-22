427564b30c994cfd4710249681d6980b29ae613e
Pratham K <prathamkumar517@gmail.com>
2022-06-22 19:51:25 +0530

    Routes and Switches explored

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>HI</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        This is the Home Page
+      </Route>
+      <Route exact path="/starred">
+        This is starred
+      </Route>
+      <Route>404 Page not found</Route>
+    </Switch>
+  );
 }
 
 export default App;
