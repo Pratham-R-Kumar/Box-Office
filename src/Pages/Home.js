f45c1ec3f8081e9e5cad1703c1af5f944294c657
Pratham K <prathamkumar517@gmail.com>
2022-06-23 13:34:07 +0530

    Added radio buttons

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,11 +5,13 @@ import { apiGet } from '../misc/config';
 const Home = () => {
   const [input, inputState] = useState('');
   const [results, setResult] = useState(null);
+  const [showOptions, setShowOptions] = useState('shows');
+  const isShowsShown = showOptions === 'shows';
   const onInputChange = ev => {
     inputState(ev.target.value);
   };
   const onSubmitSearch = () => {
-    apiGet(`/search/shows?q=${input}`).then(result => {
+    apiGet(`/search/${showOptions}?q=${input}`).then(result => {
       setResult(result);
     });
   };
@@ -25,25 +27,51 @@ const Home = () => {
     }
 
     if (results && results.length > 0) {
-      return (
-        <div>
-          {results.map(item => (
-            <div key={item.show.id}>{item.show.name}</div>
-          ))}
-        </div>
-      );
+      return results[0].show
+        ? results.map(item => <div key={item.show.id}>{item.show.name}</div>)
+        : results.map(item => (
+            <div key={item.person.id}>{item.person.name}</div>
+          ));
     }
 
     return null;
   };
+
+  const onRadioChange = ev => {
+    setShowOptions(ev.target.value);
+  };
+
   return (
     <MainPage>
       <input
         type="text"
         onChange={onInputChange}
         onKeyDown={onKeyDown}
+        placeholder="Search for something"
         value={input}
       />
+      <div>
+        <label htmlFor="search-shows">
+          Shows
+          <input
+            id="search-shows"
+            type="radio"
+            value="shows"
+            checked={isShowsShown}
+            onChange={onRadioChange}
+          />
+        </label>
+        <label htmlFor="search-actor">
+          Actors
+          <input
+            id="search-actor"
+            type="radio"
+            value="people"
+            checked={!isShowsShown}
+            onChange={onRadioChange}
+          />
+        </label>
+      </div>
       <button type="button" onClick={onSubmitSearch}>
         {' '}
         Search{' '}
