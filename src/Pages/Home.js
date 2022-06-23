d8785e699a7dcf0c4327a4b906786423c326086c
Pratham K <prathamkumar517@gmail.com>
2022-06-23 13:01:35 +0530

    Added custom render function

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
 import MainPage from '../components/MainPage';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, inputState] = useState('');
+  const [results, setResult] = useState(null);
   const onInputChange = ev => {
     inputState(ev.target.value);
   };
   const onSubmitSearch = () => {
-    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        console.log(result);
-      });
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResult(result);
+    });
   };
   const onKeyDown = ev => {
     if (ev.keyCode === 13) {
@@ -19,6 +19,23 @@ const Home = () => {
     }
   };
 
+  const displayOutput = () => {
+    if (results && results.length === 0) {
+      return <div>No result found</div>;
+    }
+
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
+  };
   return (
     <MainPage>
       <input
@@ -31,6 +48,7 @@ const Home = () => {
         {' '}
         Search{' '}
       </button>
+      {displayOutput()}
     </MainPage>
   );
 };
