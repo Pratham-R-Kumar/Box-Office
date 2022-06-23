9d2f0ba765a287eb11c64a6a55095a4128a274ca
Pratham K <prathamkumar517@gmail.com>
2022-06-23 12:33:21 +0530

    Api connected and button added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPage from '../components/MainPage';
 
 const Home = () => {
-  return <MainPage>This is Home Page</MainPage>;
+  const [input, inputState] = useState('');
+  const onInputChange = ev => {
+    inputState(ev.target.value);
+  };
+  const onSubmitSearch = () => {
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSubmitSearch();
+    }
+  };
+
+  return (
+    <MainPage>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSubmitSearch}>
+        {' '}
+        Search{' '}
+      </button>
+    </MainPage>
+  );
 };
 
 export default Home;
