af2a94da59d17d9ef2c95931628643a960684e88
Pratham K <prathamkumar517@gmail.com>
2022-06-23 21:18:43 +0530

    Added readmore option

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -5,14 +5,39 @@ import { apiGet } from '../misc/config';
 const Show = () => {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+  const [loading, isLoading] = useState(true);
+  const [error, isError] = useState(null);
 
   useEffect(() => {
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(res => {
-      setShow(res);
-    });
+    let isMounted = true;
+    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(res => {
+        if (isMounted) {
+          setShow(res);
+          isLoading(false);
+        }
+      })
+      .catch(err => {
+        if (isMounted) {
+          isError(err.message);
+          isLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
 
   console.log('show', show);
+  if (loading) {
+    return <div>Show is loading</div>;
+  }
+
+  if (error) {
+    return <div>Error occured : {error}</div>;
+  }
+
   return <div>This is the show page</div>;
 };
 
